c88e10ff0e748798aeb5fd91a9f0c3302aa15b53
Henrik Lidbjörk <henrik.lidbjork@gmail.com>
2016-12-03 12:10:57 +0100

    Yada 2

--- lidpix.py
+++ lidpix.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, session, redirect, url_for, abort, \
   render_template, flash, send_from_directory
 from wand.image import Image
-import os
+import os, string
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 # Load default config and override this config from file in env var
 app.config.update(dict(
-	PIXDIR='/home/lidbjork/public_html/photos/2011-02/',
+	PIXDIRS='./static;/home/lidbjork/public_html/photos;/home/lidbjork/Bilder/Foton',
 	SECRET_KEY='development key',
 	USERNAME='admin',
 	PASSWORD='default'
@@ -16,17 +16,17 @@ app.config.update(dict(
 app.config.from_envvar('LIDPIX_SETTINGS', silent=True)
 app.debug = 1
 
-# imagedir = app.config['PIXDIR']
-imagedir = './static/'
-thumbdir = imagedir + 'thumbs/'
+# pixdirs becomes a list of all the paths in PIXDIRS
+pixdirs = [os.path.normpath(x) for x in string.split(app.config['PIXDIRS'], ';')]
 
 
-def prep_images():
+def prep_images(directory):
 
-	""" Find all images in imagedir and make thumbnails """
+	""" Find all images in directory and make thumbnails """
 	
 	prepped = 0
-	global thumbdir, imagedir
+	directory = os.path.normpath(directory)
+	thumbdir = directory + '/thumbs/'
 	
 	if not os.path.exists(thumbdir):
 		os.mkdir(thumbdir)
@@ -34,19 +34,19 @@ def prep_images():
 	# Go through all instances in imagedir. First, check if file,
 	# then check existance as a thumb and if thumb is newer;
 	# if needed create thumbnails (sized 200*y px)
-	for imagefile in os.listdir(imagedir):
-		if os.path.isfile(imagedir + imagefile):
+	for imagefile in os.listdir(directory):
+		if os.path.isfile(directory + imagefile):
 			prepped += 1
 			if os.path.isfile(thumbdir + imagefile):
 				if (os.stat(thumbdir + imagefile).st_mtime >
-					os.stat(imagedir + imagefile).st_mtime):
+					os.stat(directory + imagefile).st_mtime):
 					continue
 			try:
-				with Image(filename = imagedir + imagefile) as img:
+				with Image(filename = directory + imagefile) as img:
 					img.transform(resize='200x')
 					img.save(filename = thumbdir + imagefile)
 			except Exception:
-				print "Error making thumb: ", imagedir+imagefile
+				pass
 			
 	return prepped
 	
@@ -63,17 +63,19 @@ def get_image_info(imagefile):
 	return exif
 	
 
+# MÅSTE nog använda pathname som ?-parameter i urlen...
 @app.route('/gallery', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	global thumbdir, imagedir
+	imagedir = pixdirs[0]
+	thumbdir = pixdirs[0] + '/thumbs/'
 	if request.method == 'POST':
 		if request.form['directory']:
 			imagedir = os.path.normpath(request.form['directory']) + '/'
 			thumbdir = imagedir + 'thumbs/'
 		return redirect(url_for('index'))
 	if request.method == 'GET':
-		prep_images()
+		prep_images(imagedir)
 		thumbs = os.listdir(thumbdir)
 		#get_image_info(imagedir + images[0])
 		return render_template('gallery.html', images=sorted(thumbs), thumbdir=thumbdir,
@@ -81,5 +83,9 @@ def index():
 								
 @app.route('/thumbs/<filename>')
 def download_file(filename):
-	print "TD: ", thumbdir
+	thumbdir = pixdirs[0] + '/thumbs/'
 	return send_from_directory(thumbdir, filename)
+
+@app.route('/test/<pathname>')
+def testpath(pathname):
+	return pathname
